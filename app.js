9eff1d55aad893a5dff0c2a4170bac34903e8a5f
dipesh guatm <dipesh@guatm.com>
2016-04-19 21:18:57 -0700

    Added multer

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var morgan = require('morgan');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var multer = require('multer');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var favicon = require('serve-favicon');
